0ea24a09b0d06b6afbbf171be1d4955b7546191c
D13GO-FG <ldiego.flores98@gmail.com>
2022-03-13 20:35:44 -0700

    Commit in conect wallet

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,12 +1,15 @@
 import Web3 from 'web3';
+// Conectamos metamask dependiendo las redes
 import { InjectedConnector } from '@web3-react/injected-connector';
 
+// Definimos las redes
 const connector = new InjectedConnector({
 	supportedChainIds: [
 		4, //Rinkeby
 	],
 });
 
+// Funcion para tener acceso a las provider de metamask
 const getLibrary = (provider) => {
 	return new Web3(provider);
 };
--- src/layouts/main/wallet-data/index.js
+++ src/layouts/main/wallet-data/index.js
@@ -15,34 +15,35 @@ import useTruncatedAddress from '../../../hooks/useTruncatedAddress';
 
 const WalletData = () => {
 	const [balance, setBalance] = useState(0);
+	// Funcion para detectar si esta activa la cuenta, la red y otras configuraciones
 	const { active, activate, deactivate, account, error, library } =
 		useWeb3React();
-
+	// En caso de ser una red no soportada
 	const isUnsupportedChain = error instanceof UnsupportedChainIdError;
-
+	// Constante para conocer el estado de la conexion (activo/no activo)
 	const connect = useCallback(() => {
 		activate(connector);
 		localStorage.setItem('previouslyConnected', 'true');
 	}, [activate]);
-
+	// En caso de desconectar cambia el estado del storage del browser
 	const disconnect = () => {
 		deactivate();
 		localStorage.removeItem('previouslyConnected');
 	};
-
+	// Optener los datos del usuario a travez de la wallet
 	const getBalance = useCallback(async () => {
 		const toSet = await library.eth.getBalance(account);
 		setBalance((toSet / 1e18).toFixed(2));
 	}, [library?.eth, account]);
-
+	// Si esta conectado en el provider
 	useEffect(() => {
 		if (active) getBalance();
 	}, [active, getBalance]);
-
+	// Si esta acito en el storage
 	useEffect(() => {
 		if (localStorage.getItem('previouslyConnected') === 'true') connect();
 	}, [connect]);
-
+	// Recortar public key en la interface
 	const truncatedAddress = useTruncatedAddress(account);
 
 	return (
